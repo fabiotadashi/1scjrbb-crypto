0f85a8735eacb4c553a731f656fe00101e15c913
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2022-05-25 22:06:41 -0300

    query parameter

--- src/main/java/br/com/fiap/cryptobb/CryptoController.java
+++ src/main/java/br/com/fiap/cryptobb/CryptoController.java
@@ -1,14 +1,38 @@
 package br.com.fiap.cryptobb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequestMapping("cryptos")
 public class CryptoController {
 
+    private List<CryptoDTO> cryptoDTOList;
+
+    public CryptoController(){
+        cryptoDTOList =  Arrays.asList(
+                new CryptoDTO(1, "BitCoin", "BTC", BigDecimal.valueOf(150_000)),
+                new CryptoDTO(2, "Etherium", "ETH", BigDecimal.valueOf(10_000)),
+                new CryptoDTO(3, "Dodge Coin", "DODGE", BigDecimal.valueOf(3)),
+                new CryptoDTO(4, "Cardano", "ADA", BigDecimal.valueOf(3.5))
+        );
+    }
+
     @GetMapping
-    public String hello(){
-        return "Hello World";
+    public List<CryptoDTO> listCryptos(
+            @RequestParam(required = false) String name
+    ) {
+        return cryptoDTOList.stream()
+                .filter(cryptoDTO -> name == null || cryptoDTO.getName().contains(name))
+                .collect(Collectors.toList());
     }
 
 }
---
+++ src/main/java/br/com/fiap/cryptobb/CryptoDTO.java
@@ -0,0 +1,52 @@
+package br.com.fiap.cryptobb;
+
+import java.math.BigDecimal;
+
+public class CryptoDTO {
+
+    private int id;
+    private String name;
+    private String acronym;
+    private BigDecimal usdValue;
+
+    public CryptoDTO(){}
+
+    public CryptoDTO(int id, String name, String acronym, BigDecimal usdValue) {
+        this.id = id;
+        this.name = name;
+        this.acronym = acronym;
+        this.usdValue = usdValue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public BigDecimal getUsdValue() {
+        return usdValue;
+    }
+
+    public void setUsdValue(BigDecimal usdValue) {
+        this.usdValue = usdValue;
+    }
+}
