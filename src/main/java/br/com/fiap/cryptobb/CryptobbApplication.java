0da7b1b9e6da6f957a67fb6eef35709680617ef2
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2022-06-01 20:49:00 -0300

    security configurer adapter

--- src/main/java/br/com/fiap/cryptobb/CryptobbApplication.java
+++ src/main/java/br/com/fiap/cryptobb/CryptobbApplication.java
@@ -2,8 +2,9 @@ package br.com.fiap.cryptobb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CryptobbApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/fiap/cryptobb/configuration/SecurityConfig.java
@@ -0,0 +1,50 @@
+package br.com.fiap.cryptobb.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}senha").roles("USER")
+                    .and()
+                .withUser("admin").password("{noop}admin").roles("USER","ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/cryptos").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(
+                        "/v3/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**"
+                );
+    }
+
+}
