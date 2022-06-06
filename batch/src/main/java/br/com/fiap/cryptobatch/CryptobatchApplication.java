16e1db4c97a95909074322ddc6673768cdfab588
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2022-06-06 20:04:03 -0300

    start batch

---
+++ batch/src/main/java/br/com/fiap/cryptobatch/CryptobatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cryptobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptobatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptobatchApplication.class, args);
+	}
+
+}
