ad4e06e3b5aa77efe799d1428fffd53be971d2bf
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2022-06-06 20:06:06 -0300

    fix gitignore

--- web/build/resources/main/db/migration/V1_000__create_table_crypto.sql
+++
@@ -1,8 +0,0 @@
-create table TB_CRYPTOS(
-  id int auto_increment primary key,
-  name varchar(200),
-  acronym varchar(30),
-  usd_value double,
-  created_date timestamp not null,
-  last_updated_date timestamp
-);
--- web/build/resources/main/db/migration/V1_003__fix_last_date_user.sql
+++
@@ -1,2 +0,0 @@
-alter table TB_USERS drop column last_updated_date;
-alter table TB_USERS add column last_modified_date timestamp not null;
